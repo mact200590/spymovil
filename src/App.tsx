7dd5cf5b6026d4d730dbbcb782cf2558fa0d3342
Fabian Alvariza <falvariza@kubikware.com>
2019-10-11 22:30:35 -0300

    add ThemeProvider from m-ui

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SignIn from './page/SignIn';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <SignIn/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { ThemeProvider } from "@material-ui/styles";
+import { CssBaseline } from "@material-ui/core";
+import theme from "./style/theme";
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <App />
+  </ThemeProvider>,
+  document.getElementById("root")
+);
+serviceWorker.unregister();
