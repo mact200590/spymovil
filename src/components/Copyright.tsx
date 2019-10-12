d59962d83f3b2699fbc8b4b1f8674e5ce5b8d01a
Fabian Alvariza <falvariza@kubikware.com>
2019-10-12 01:29:52 -0300

    add Copyright component

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,16 @@
+import Link from "@material-ui/core/Link";
+import Typography from "@material-ui/core/Typography";
+import React from "react";
+
+export const Copyright = () => {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"PedidosYa © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        2010-2019
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+};
