be08282af13e61707b5a0b3f7bb4f92002e5ea11
Yanier Alfonso <yanier.alfonso@gmail.com>
2019-10-12 13:15:50 -0300

    testing restaurant row

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { PYRestaurantRow } from './components/PYRestaurantRow';
 import SignIn from './page/SignIn';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <SignIn/>
+      <PYRestaurantRow
+      name={"Va como pina"}
+      deliveryTimeMaxMinutes={60}
+      link="http://www.pedidosya.com.uy/restaurantes/montevideo/<link>-menu"
+      pathLogo="pathlog"
+      rating={3.4}
+      topCategories={["jama","baile","cosa gorda"]}
+      />
     </div>
   );
 }
