e8b36a5884ccf914ebf3df5f1d4f23caab43dbcf
Yanier Alfonso <yanier.alfonso@gmail.com>
2019-10-16 22:47:45 -0300

    creating hooks to fetch pedidosYa api

--- src/hooks/fetch.ts
+++ src/hooks/fetch.ts
@@ -1,28 +1,101 @@
-import { useMemo, useState } from "react";
-import useFetch from "react-fetch-hook";
-import { Params, PathApi, UNDEFINE_PARAMS_MESSAGE_ERROR } from "../types";
+import { useEffect, useMemo, useState } from "react";
+import { Params, PathApi, UNDEFINED_PARAMS_MESSAGE_ERROR } from "../types";
 import { getHeaders, saveAuth } from "../utils/auth";
 import { API_URL } from "../utils/constants";
 
-export const useFetchPedidosYaApiTest = ({
-  pathApi,
-  params
-}: {
-  pathApi: PathApi;
-  params?: Params;
-}) => {
+export const useFetchPedidosYaApiTest = (
+  {
+    pathApi,
+    params
+  }: {
+    pathApi: PathApi;
+    params?: Params;
+  },
+  depends: any[]
+) => {
+  const [data, setData] = useState(undefined);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState(undefined);
   const { url, error: errorParams } = useGenerateApiUrl({ pathApi, params });
-  const { isLoading, data, error } = useFetch(url, { headers: getHeaders() });
-  if (
-    !isLoading &&
-    !error &&
-    !errorParams &&
-    pathApi.match("/tokens") &&
-    data
-  ) {
-    saveAuth(data);
-  }
-  return { isLoading, data, error, errorParams };
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(url, { headers: getHeaders() })
+      .then(j => j.json())
+      .then(data => {
+        setIsLoading(false);
+        if (data.code) {
+          setError(data);
+        } else {
+          setData(data);
+        }
+        if (pathApi.match("/tokens") && data) {
+          saveAuth(data);
+        }
+        console.log(`Fetch url: ${url} `, { data });
+      })
+      .catch(error => {
+        error && console.log(`Fetch url: ${url}`, { error });
+        setError(error);
+        setIsLoading(false);
+      });
+  }, depends);
+  return {
+    data,
+    isLoading,
+    error,
+    errorParams
+  };
+};
+
+export const useFetchPedidosYaApiTestLazy = () => {
+  const [data, setData] = useState(undefined);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState(undefined);
+  const [{ params, pathApi, count }, setArgs] = useState<{
+    pathApi: PathApi;
+    params?: Params;
+    count: number;
+  }>({ pathApi: "/tokens/app", count: 0 });
+  const { url, error: errorParams } = useGenerateApiUrl({ pathApi, params });
+  useEffect(() => {
+    if (count === 0) return;
+    setIsLoading(true);
+    fetch(url, { headers: getHeaders() })
+      .then(j => j.json())
+      .then(data => {
+        setIsLoading(false);
+        if (data.code) {
+          setError(data);
+        } else {
+          setData(data);
+        }
+        if (pathApi.match("/tokens") && data) {
+          saveAuth(data);
+        }
+        console.log(`FetchLazy url: ${url} `, { data });
+      })
+      .catch(error => {
+        error && console.log(`FetchLazy url: ${url}`, { error });
+        setError(error);
+        setIsLoading(false);
+      });
+  }, [params, pathApi, count]);
+  const trigger = ({
+    pathApi,
+    params
+  }: {
+    pathApi: PathApi;
+    params?: Params;
+  }) => {
+    setArgs({ pathApi, params, count: count + 1 });
+  };
+  return {
+    trigger,
+    data,
+    isLoading,
+    error,
+    errorParams
+  };
 };
 
 const useGenerateApiUrl = ({
@@ -37,9 +110,13 @@ const useGenerateApiUrl = ({
     let url = `${API_URL}${pathApi}`;
     if (!params) return url;
     const keys = Object.keys(params);
-    url = keys.length > 0 ? url.concat("?") : url;
+    if (keys.length > 0) {
+      url = url.concat("?");
+    } else {
+      return url;
+    }
     return keys.reduce((pre, curr, currIndex) => {
-      if (!(params as any)[curr]) setError(UNDEFINE_PARAMS_MESSAGE_ERROR);
+      if (!(params as any)[curr]) setError(UNDEFINED_PARAMS_MESSAGE_ERROR);
       const newCurr = `${pre}${curr}=${(params as any)[curr]}`;
       const separator = keys.length - 1 !== currIndex ? "&" : "";
       return `${newCurr}${separator}`;
