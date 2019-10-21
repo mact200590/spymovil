88cb53d586437a05fb2921b0ef0c39a520843bf7
Manuel Alejandro cabriales <mact200590@gmail.com>
2019-10-21 12:15:56 -0300

    fix-everytihng

--- src/test/PYButton.test.js
+++ src/test/PYButton.test.js
@@ -1,55 +1,44 @@
-import '@testing-library/jest-dom/extend-expect';
-import { cleanup, render } from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
+import { cleanup, render } from "@testing-library/react";
 import React from "react";
 import ReactDOM from "react-dom";
-// import renderer from 'react-test-renderer';
+import { act, create } from "react-test-renderer";
 import { PYButton } from "../components/PYButton";
-import TestRenderer from 'react-test-renderer';
-import {act} from "@testing-library/dom"
 
-
-afterEach(cleanup)
-import { format } from 'path';
+afterEach(cleanup);
 it("Button with all mandatory props  or other props ", () => {
-   const div=document.createElement('div')
-    ReactDOM.render(<PYButton
-            label="Buscarte"
-            typeVariant="primary"
-            fullWidth={false}
-        />,
-            div);
-       
+  const div = document.createElement("div");
+  ReactDOM.render(
+    <PYButton label="Buscarte" typeVariant="primary" fullWidth={false} />,
+    div
+  );
 });
 
-it("It renders correctly",()=>{
-    const {getByTestId}=render(<div data-testid='button'><PYButton 
-           label="Buscar"
-           typeVariant="primary"
-           fullWidth={false}    
-    /></div>);
-    expect(getByTestId('button')).toHaveTextContent("Buscar");
-
+it("It renders correctly", () => {
+  const { getByTestId } = render(
+    <div data-testid="button">
+      <PYButton label="Buscar" typeVariant="primary" fullWidth={false} />
+    </div>
+  );
+  expect(getByTestId("button")).toHaveTextContent("Buscar");
 });
-
-
-it("Matches Snapshot",()=>{
-    const tree=TestRenderer.create(
-    <div data-testid='button'>
-      <PYButton label="Buscar" typeVariant="primary" fullWidth={false}/>
-    </div>).toJSON(); 
-expect(tree).toMatchSnapshot();
-})
 
 it("Matches Snapshot", () => {
-    let tree;
-    
-    act(() => {
-      tree = TestRenderer.create(
-        <div data-testid="button">
-          <PYButton label="Buscar" typeVariant="primary" fullWidth={false} />
-        </div>
-      );
-    });
-  
-    expect(tree.toJSON()).toMatchSnapshot();
+  let tree;
+  act(() => {
+    tree = create(
+      <div data-testid="button">
+        <PYButton label="Buscar" typeVariant="primary" fullWidth={false} />
+      </div>
+    );
   });
+
+  expect(tree.toJSON()).toMatchSnapshot();
+});
+
+it("Matches snapshot with React Testing Library", () => {
+  const { container } = render(
+    <PYButton label="Buscar" typeVariant="primary" fullWidth={false} />
+  );
+  expect(container).toMatchSnapshot();
+});
