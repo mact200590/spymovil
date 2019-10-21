f45b3f2eaf6f2f2b7fd3517dc916d0fb41a26bbf
Manuel Alejandro cabriales <mact200590@gmail.com>
2019-10-21 11:18:28 -0300

    add-test

--- src/test/PYButton.test.js
+++ src/test/PYButton.test.js
@@ -2,11 +2,14 @@ import '@testing-library/jest-dom/extend-expect';
 import { cleanup, render } from '@testing-library/react';
 import React from "react";
 import ReactDOM from "react-dom";
-import renderer from 'react-test-renderer';
+// import renderer from 'react-test-renderer';
 import { PYButton } from "../components/PYButton";
-import { act } from "react-dom/test-utils";
+import TestRenderer from 'react-test-renderer';
+import {act} from "@testing-library/dom"
+
 
 afterEach(cleanup)
+import { format } from 'path';
 it("Button with all mandatory props  or other props ", () => {
    const div=document.createElement('div')
     ReactDOM.render(<PYButton
@@ -30,7 +33,7 @@ it("It renders correctly",()=>{
 
 
 it("Matches Snapshot",()=>{
-    const tree=renderer.create(
+    const tree=TestRenderer.create(
     <div data-testid='button'>
       <PYButton label="Buscar" typeVariant="primary" fullWidth={false}/>
     </div>).toJSON(); 
@@ -39,8 +42,9 @@ expect(tree).toMatchSnapshot();
 
 it("Matches Snapshot", () => {
     let tree;
+    
     act(() => {
-      tree = create(
+      tree = TestRenderer.create(
         <div data-testid="button">
           <PYButton label="Buscar" typeVariant="primary" fullWidth={false} />
         </div>
