e947ca20bebb78b47b19ac4f9a1ee365be8e6768
Manuel Alejandro Cabriales <mact200590@gmail.com>
2019-12-05 10:21:27 -0300

    addTextComponent

---
+++ src/components/GDText.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/styles';
+
+interface Props {
+    title: string;
+    titleSubtitle?: string;
+}
+
+const GDText = ({ title, titleSubtitle }: Props) => {
+    const classes = useStyles();
+    return (
+        <div className={classes.container}>
+            <p className={classes.title}>{title}</p>
+            <p className={classes.titleSubtitle}>{titleSubtitle}</p>
+        </div>
+    )
+}
+
+
+export default GDText;
+
+const useStyles = makeStyles(theme => ({
+    container: {
+        display: "flex",
+        flexDirection: "column",
+    },
+    title: {
+        fontFamily: "Times New Roman",
+        fontSize: 40,
+        marginBottom:5
+    },
+    titleSubtitle: {
+        fontFamily: "Times New Roman",
+        fontSize: 20
+    }
+}));
