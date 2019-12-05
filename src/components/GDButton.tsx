448652b9d800f1825fcf632c5b658c80cebdf069
Manuel Alejandro Cabriales <mact200590@gmail.com>
2019-12-05 14:36:43 -0300

    add-correct-name-button

---
+++ src/components/GDButton.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@material-ui/core";
+import { ButtonBaseProps } from "@material-ui/core/ButtonBase";
+import React, { useMemo } from "react";
+
+type ButtonType = "primary" | "secondary";
+
+type Props = {
+  label: string;
+  typeVariant: ButtonType;
+  fullWidth: boolean;
+} & ButtonBaseProps;
+
+export const GDButton: React.FC<Props> = ({
+  label,
+  typeVariant,
+  fullWidth,
+  ...rest
+}) => {
+  const variant = useMemo(
+    () => ({ primary: "contained", secondary: "outlined" }[typeVariant] as any),
+    [typeVariant]
+  );
+  return (
+    <Button
+      fullWidth={fullWidth}
+      variant={variant}
+      {...rest}
+      color="primary"
+    >
+      {label}
+    </Button>
+  );
+};
+
