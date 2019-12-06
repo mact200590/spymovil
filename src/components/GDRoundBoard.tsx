18a22a3ce439e679bf4ca966f3d4ebc5499ea5ed
Yanier Alfonso <yanier.alfonso@gmail.com>
2019-12-05 22:25:48 -0300

    refactory GDRoundBoard

---
+++ src/components/GDRoundBoard.tsx
@@ -0,0 +1,93 @@
+import { Theme, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { GDButton } from "./GDButton";
+import GDSelectLabel from "./GDSelectLabel";
+
+interface Props {
+  numberRound: number;
+  players: string[];
+  onResult?: (player: string) => void;
+}
+
+interface InfoMove {
+  player: string;
+  move: string;
+}
+
+const GDRoundBoard = ({ numberRound, players, onResult }: Props) => {
+  const classes = useStyles();
+  const [indexActive, setIndexActive] = useState(0);
+  const [moveActive, setMoveActive] = useState("Scissors");
+  const [infoMoves, setInfoMoves] = useState<InfoMove[]>([]);
+  const onClick = useCallback(() => {
+    setInfoMoves([
+      ...infoMoves,
+      {
+        move: moveActive,
+        player: players[indexActive]
+      }
+    ]);
+    if (indexActive < players.length - 1) {
+      setIndexActive(indexActive + 1);
+    } else {
+      //TODO: calc Result is pending
+      onResult && onResult("Ale");
+      setIndexActive(0);
+      setInfoMoves([]);
+    }
+  }, [moveActive, indexActive, players]);
+  const labelButton = useMemo(() => {
+    return indexActive < players.length - 1 ? "next" : "play";
+  }, [indexActive, players]);
+  useEffect(() => {
+    console.log({ infoMoves });
+    setMoveActive("Scissors");
+  }, [infoMoves]);
+
+  return (
+    <div className={classes.container}>
+      <Typography variant="h4" color={"textSecondary"} align="center">
+        {`ROUND: ${numberRound}`}
+      </Typography>
+
+      <div className={classes.containerSelect}>
+        <Typography variant="h4" color={"textSecondary"}>
+          {`Player: ${players[indexActive]}`}
+        </Typography>
+        <GDSelectLabel
+          label={"Select Move"}
+          onChange={setMoveActive}
+          options={["Scissors", "Paper", "Rock"]}
+          typeVariant={"primary"}
+        />
+      </div>
+      <GDButton
+        className={classes.button}
+        label={labelButton}
+        typeVariant="primary"
+        fullWidth={false}
+        type="submit"
+        onClick={onClick}
+      />
+    </div>
+  );
+};
+
+export default GDRoundBoard;
+
+const useStyles = makeStyles((theme: Theme) => ({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center"
+  },
+  containerSelect: {
+    display: "flex",
+    flexDirection: "column",
+    marginTop: theme.spacing(10)
+  },
+  button: {
+    marginTop: theme.spacing(20)
+  }
+}));
--- src/components/GDUnionRoundScoreView.tsx
+++ src/components/GDUnionRoundScoreView.tsx
@@ -1,41 +1,53 @@
-import React from 'react'
-import GDScoreBoard from './GDScoreBoard'
-import GDRoundView from './GDRoudView'
-import { makeStyles } from '@material-ui/styles'
+import React, { useState } from "react";
+import GDScoreBoard from "./GDScoreBoard";
+import GDRoundBoard from "./GDRoundBoard";
+import { makeStyles } from "@material-ui/styles";
 
 interface Props {
-    options: string[]
-    numberRound: number
-    labelButton: string
-    activePlayer: string
-    tittleBoard: string
-    scores: { score: number, winner: string }[]
+  players: string[];
 }
 
-const GDUnionRoundScoreView = ({ options, numberRound, labelButton, activePlayer, tittleBoard }: Props) => {
-    const classes = useStyles();
-    return (
-        <div className={classes.container}>
-            <div>
-                <GDRoundView options={options} numberRound={numberRound} labelButton={labelButton} activePlayer={activePlayer} />
-            </div>
-            <div className={classes.score}>
-                <GDScoreBoard tittle={"Score Board"} scores={[{ score: 1, winner: "Alejandro" }, { score: 1, winner: "Alejandro" }, { score: 1, winner: "Alejandro" }, { score: 1, winner: "Alejandro" }, { score: 1, winner: "Alejandro" }, { score: 1, winner: "Alejandro" }, { score: 1, winner: "Alejandro" }, { score: 1, winner: "Alejandro" }, { score: 1, winner: "Alejandro" }, { score: 1, winner: "Alejandro" }, { score: 1, winner: "Alejandro" }, { score: 1, winner: "Alejandro" }, { score: 1, winner: "Alejandro" }, { score: 1, winner: "Alejandro" }, { score: 1, winner: "Alejandro" }, { score: 1, winner: "Alejandro" }, { score: 1, winner: "Alejandro" }, { score: 1, winner: "Alejandro" }, { score: 1, winner: "Alejandro" }]} />
-            </div>
-        </div>
-    )
-}
+const GDUnionRoundScoreView = ({ players }: Props) => {
+  const [numberRoundActive, setNumberRoundActive] = useState(1);
+  const classes = useStyles();
+  return (
+    <div className={classes.container}>
+      <div>
+        <GDRoundBoard
+          numberRound={numberRoundActive}
+          players={players}
+          onResult={winner => {
+            console.log(winner);
+          }}
+        />
+      </div>
+      <div className={classes.score}>
+        <GDScoreBoard
+          tittle={"Score Board"}
+          scores={[
+            { score: 1, winner: "Alejandro" },
+            { score: 1, winner: "Alejandro" },
+            { score: 1, winner: "Alejandro" },
+            { score: 1, winner: "Alejandro" },
+            { score: 1, winner: "Alejandro" },
+            { score: 1, winner: "Alejandro" }
+          ]}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default GDUnionRoundScoreView;
 
 const useStyles = makeStyles(theme => ({
-    container: {
-        display: "flex",
-        flexDirection: "row",
-        paddingLeft: 10,
-        justifyContent: 'center',
-    },
-    score: {
-        paddingLeft: 90
-    }
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    paddingLeft: 10,
+    justifyContent: "center"
+  },
+  score: {
+    paddingLeft: 90
+  }
 }));
