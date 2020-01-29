323a4be5e23096090da056c3fae878122aae6189
Manuel Alejandro Cabriales Toledo <mact200590@gmail.com>
2020-01-29 14:08:20 -0300

    addConst format-fieldsOfApi

---
+++ src/utils/conts.ts
@@ -0,0 +1,2 @@
+export const DATE_FORMAT="MM/dd/yyyy"
+export const FIELDS_API= ["Very acid","Moderately acidic","Slightly acidic","Neutral","Slightly alkaline","Moderately alkaline","Very alkaline"]
