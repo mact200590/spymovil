0094924220fe72a6a8df49034e7d8cc33b23885a
Manuel Alejandro Cabriales Toledo <mact200590@gmail.com>
2020-01-29 20:48:44 -0300

    change-lenguague

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,2 +1,2 @@
 export const DATE_FORMAT="MM/dd/yyyy"
-export const FIELDS_API= ["Very acid","Moderately acidic","Slightly acidic","Neutral","Slightly alkaline","Moderately alkaline","Very alkaline"]
+export const FIELDS_API= ["Muy ácido","Moderadamente ácido","Ligeramente ácido","Neutro","Ligeramente alcalino","Moderadamente alcalino","Muy alcalino"]
