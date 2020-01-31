9f65dff092ba59e93a784804907b830fd03611d8
Manuel Alejandro Cabriales Toledo <mact200590@gmail.com>
2020-01-31 18:46:43 -0300

    pass-style-FilterComponent

--- src/components/SPYFilters.tsx
+++ src/components/SPYFilters.tsx
@@ -7,6 +7,8 @@ import SPYText from './SPYText'
 import { FIELDS_API } from '../utils/constant'
 import { TURBIDITY_OF_WATER } from '../utils/constant'
 import SPYDateSelector from './SPYDateSelector'
+import theme from "../style/theme"
+
 
 interface Props {
     onClick: (name: string, chlorine: string, turbidity: string, date: string, type: string, selectPh: string) => void
@@ -53,30 +55,33 @@ const SPYFilters = ({ onClickClear, onClick }: Props) => {
             <SPYText title={'Filters'} />
             <div className={classes.filters}>
                 <SPYInput
+                    classNameContainer={classes.input}
                     typeVariant="primary"
                     placeholder="Nombre"
                     label="Nombre"
                     value={name}
                     onChange={handleOnchangeName}
                 />
-                <SPYInput
+                  <SPYInput
+                   classNameContainer={classes.input}
                     typeVariant="primary"
                     placeholder="Cloro"
                     label="Cloro"
                     value={chlorine}
                     onChange={handleOnchangeChlorine}
-                />
+                /> 
                 <SPYSelect
                     label={"Turbidez"}
-                    styleContainer={{ marginTop: "0px" }}
+                    styleContainer={{ marginTop: "0px", minWidth: "150px"}}
                     typeVariant={"primary"}
                     options={TURBIDITY_OF_WATER}
                     value={turbidity}
                     onChange={handleOnchangeSelectTurbidity}
                 />
                 <SPYDateSelector
+                    classNameContainer={classes.date}
                     variant={"inline"}
-                    justify={"center"}
+                    justify={"flex-start"}
                     disableToolbar={false}
                     id={"id-ph"}
                     label={"Elija la fecha"}
@@ -84,25 +89,26 @@ const SPYFilters = ({ onClickClear, onClick }: Props) => {
                 />
 
                 <SPYInput
+                    classNameContainer={classes.input}
                     typeVariant="primary"
                     placeholder="Tipo"
                     label="Tipo"
                     value={type}
                     onChange={handleOnchangeType}
                 />
-                <SPYSelect
+               <SPYSelect
+                     styleContainer={{ marginTop: "0px", minWidth: "150px"}}
                     label={"Elija el Ph"}
-                    styleContainer={{ marginTop: "0px" }}
                     typeVariant={"primary"}
                     options={FIELDS_API}
                     value={selectPh}
                     onChange={handleOnchangeSelectPh}
                 />
-                <div className={classes.button}>
-                    <SPYButton style={{ paddingRight: "15px" }}
+               <div className={classes.button}>
+                    <SPYButton  style={{ marginRight: "15px" }}
                         label={'Filtrar'} typeVariant={'primary'} fullWidth={false} onClick={() => onClick(name, chlorine, selectPh, turbidity, date, type)} />
-                    <SPYButton label={'Borrar'} typeVariant={'primary'} fullWidth={false} onClick={() => onClickClear} />
-                </div>
+                    <SPYButton style={{ marginRight: "15px", backgroundColor:theme.palette.secondary.main }} label={'Borrar'} typeVariant={'primary'} fullWidth={false} onClick={() => onClickClear} />
+                </div> 
             </div>
         </div>
     )
@@ -118,15 +124,23 @@ const useStyles = makeStyles(theme => ({
     filters: {
         display: "flex",
         flexDirection: "row",
-        minWidth: "400px"
+        width:"100%"
     },
     button: {
         marginTop: "10px",
-        marginRight: "50px",
+        marginLeft: "10px",
         maxHeight: "40px",
         display: "flex",
         flexDirection: "row",
         width: "100%"
+    },
+    input:{
+      marginRight:"10px",
+      minWidth: "150px"
+    },
+    date:{
+        maxWidth: "150px",
+        marginRight: "10px",
     }
 }));
 
