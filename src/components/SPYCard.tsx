5044a4fe87ca7bc67addaff65eab308e46149f84
Manul Alejandro Cabriales Toledo <mact200590@gmail.com>
2020-02-13 23:47:53 -0300

    card finished

--- src/components/SPYCard.tsx
+++ src/components/SPYCard.tsx
@@ -1,13 +1,12 @@
 import { Container } from "@material-ui/core";
 import Avatar from "@material-ui/core/Avatar";
 import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
 import CardMedia from "@material-ui/core/CardMedia";
-import { red } from "@material-ui/core/colors";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import moment from "moment";
 import React from "react";
+import { phValueToLabel } from "../utils/helper";
 
 interface Plants {
   id: number;
@@ -22,7 +21,6 @@ export interface DataApi {
   turbidity: number;
   date: string;
   type: Plants;
-
 }
 
 interface Props {
@@ -30,57 +28,66 @@ interface Props {
 }
 
 const SYPCard = ({
+  dataApi,
   dataApi: { id, name, chlorine, ph, turbidity, date, type }
 }: Props) => {
-  const classes = useStyles();
+  const classes = useStyles({ dataApi });
   const [expanded, setExpanded] = React.useState(false);
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
-  
-  const img=(id.toString()).concat(`.jpg`);
-  const image="/assets/images/"
- const resut= image.concat(img)
 
-  return (  
+  const imagePath = `/assets/images/${id}.jpg`;
+
+  return (
     <Card className={classes.root}>
-      <CardMedia
-        className={classes.media}
-        image={resut}
-        title="Paella dish"
-      />
+      <Typography
+        gutterBottom
+        variant="h5"
+        component="h2"
+        className={classes.name}
+      >
+        {name}
+      </Typography>
+      <CardMedia className={classes.media} image={imagePath} title="Paella dish" />
+
       <Container className={classes.nameContainer}>
         <Typography
-          gutterBottom
-          variant="h5"
-          component="h2"
-          className={classes.name}
+          className={classes.date}
+          variant="body2"
+          color="textSecondary"
+          component="p"
         >
-          {name}
+          {moment(date).format("DD MMM, YYYY")}
         </Typography>
-        <CardHeader
-          avatar={
-            <Avatar variant="rounded" className={classes.avatar}>
-              {""}
-            </Avatar>
-          }
-          title="Ph"
+        <Avatar variant="rounded" className={classes.avatar}>
+          {""}
+        </Avatar>
+        <Typography
+          variant="body2"
+          color="textSecondary"
+          component="p"
           className={classes.ph}
-        />
+        >
+          Ph
+        </Typography>
       </Container>
-      <Typography className={classes.date} variant="body2" color="textSecondary" component="p">
-        {moment(date).format("DD, MMMM, YYYY")}
-      </Typography>
-      <Typography  className={classes.itemValue}variant="body2" color="textSecondary" component="p">
-              <Typography variant="body2" color="textSecondary" component="p">
-          {`Chlorine:${chlorine}`}
+
+      <Typography
+        className={classes.itemValue}
+        variant="body2"
+        color="textSecondary"
+        component="p"
+      >
+        <Typography variant="body2" color="textSecondary" component="p">
+          {`Chlorine: ${chlorine}`}
         </Typography>
         <Typography variant="body2" color="textSecondary" component="p">
-          {`Turbidity:${turbidity}`}
+          {`Turbidity: ${turbidity}`}
         </Typography>
         <Typography variant="body2" color="textSecondary" component="p">
-          {`Type:${type.name}`}
+          {`Type: ${type.name}`}
         </Typography>
       </Typography>
     </Card>
@@ -90,31 +97,32 @@ const SYPCard = ({
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      width: "100%"
+      width: "100%",
+      borderWidth: 0,
+      boxShadow: "0px 0px 0px #FFFFFF"
     },
     nameContainer: {
       display: "flex",
-      justifyContent: "space-between",
       paddingLeft: 0,
       paddingRight: 0,
       paddingBottom: 0
     },
     name: {
-      marginBottom: 0
+      marginBottom: 0,
+      textShadow: "1px 2px 3px #3e3b3b",
+      color: "#3a3838"
     },
     ph: {
-      paddingTop: 2,
-      paddingLeft: 0,
-      paddingRight: 0,
-      paddingBottom: 2
+      alignSelf: "center",
+      marginLeft: 5
     },
     media: {
       height: "100%",
       width: "100%",
-      minHeight: 150,
+      minHeight: 250,
       minWidth: 350,
       borderRadius: 5
-        },
+    },
     expand: {
       transform: "rotate(0deg)",
       marginLeft: "auto",
@@ -125,25 +133,26 @@ const useStyles = makeStyles((theme: Theme) =>
     expandOpen: {
       transform: "rotate(180deg)"
     },
-    avatar: {
-      width: 50,
-      height: 5,
-      backgroundColor: red[500],
-      paddingRight:15,
-    }, 
-    date:{
-      borderRadius:5,
-      maxWidth:150,
-      border: 2, 
-      borderStyle:"solid", 
-      borderColor:"#858585",
-      opacity:1,
-      paddingLeft:5,
-      marginTop:5
-
+    avatar: (props: Props) => ({
+      width: 10,
+      height: 10,
+      backgroundColor: phValueToLabel(props.dataApi.ph).color,
+      alignSelf: "center",
+      marginLeft: 15
+    }),
+    date: {
+      borderRadius: 5,
+      maxWidth: 150,
+      border: 2,
+      borderStyle: "solid",
+      borderColor: "#858585",
+      opacity: 1,
+      paddingLeft: 5,
+      paddingRight: 5,
+      marginTop: 5
     },
-    itemValue:{
-     paddingLeft:5
+    itemValue: {
+      paddingLeft: 5
     }
   })
 );
